4827e4162d69e015335d9fa2ea201a3c75d51102
Iryna.biruk <Iryna.biruk@gmail.com>
2019-10-17 12:37:40 +0300

    lesson6A

--- src/by/it/biruk/lesson06/TaskA1.java
+++ src/by/it/biruk/lesson06/TaskA1.java
@@ -29,18 +29,16 @@ package by.it.biruk.lesson06;
 
 
 public class TaskA1 {
-    public void main(String[] args) {
-        class Dog {
-            String name;
-
-            Dog(String name){
-                /*public void s
-                this.name=name;
-                public String get.Name(){
-                    return name;*/
-                }
-            }
-        }
+    public static void main(String[] args) {
+        Dog dog1=new Dog();
+        dog1.setName("Шарик");
+        dog1.setAge(5);
+        Dog dog2=new Dog();
+        dog2.setName("Тузик");
+        dog2.setAge(3);
+        System.out.println(dog1.getName()+ " "+dog1.getAge());
+        System.out.println(dog2.getName()+ " "+dog2.getAge());
+    }
     }
 
 
