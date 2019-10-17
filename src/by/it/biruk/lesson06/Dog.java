fe80ea2b3e6733450e274921f535e95c2acec4a2
Iryna Biruk <Iryna.biruk@gmail.com>
2019-10-17 23:23:18 +0300

    lesson06

--- src/by/it/biruk/lesson06/Dog.java
+++ src/by/it/biruk/lesson06/Dog.java
@@ -4,32 +4,32 @@ public class Dog {
     private String name;
     private int age;
 
-    public Dog() {
+    public Dog () {
     }
 
-    public Dog(String name, int age) {
+    public Dog (String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public String getName() {
+    public String getName () {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName (String name) {
         this.name = name;
     }
 
-    public int getAge() {
+    public int getAge () {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge (int age) {
         this.age = age;
     }
 
     @Override
-    public String toString() {
-        return "Кличка: "+ name +". Возраст: "+ age;
+    public String toString () {
+        return "Кличка: " + name + ". Возраст: " + age;
     }
 }
--- src/by/it/biruk/lesson06/TaskB1.java
+++ src/by/it/biruk/lesson06/TaskB1.java
@@ -2,14 +2,6 @@ package by.it.biruk.lesson06;
 
 /*
 
-B. (продолжение задач уровня А).
-
-Создайте новый класс DogHelper и в нем два статических метода
-void printAllNames(Dog[] dogs){.....}; //должен печатать в консоль через пробел имена всех собак
-double averageAge(Dog[] dogs){.....}; //должен вычислять средний возраст переданных ему собак
-
-В классе TaskB1 и его методе main
-    Создайте массив из 5 разных собак. Дайте всем клички и укажите возраст.
     Шарик 1 год, Жучка 2 года, Бобик 3 года, Барбос 4 года, Полкан 5 лет
 
     Напечатайте их имена с помощью метода printAllNames, ожидается
@@ -26,6 +18,16 @@ double averageAge(Dog[] dogs){.....}; //должен вычислять сред
 */
 
 public class TaskB1 {
-
+    public static void main (String[] args) {
+        Dog[] dogs = {new Dog("Шарик", 1),
+                new Dog("Жучка", 2),
+                new Dog("Бобик", 3),
+                new Dog("Барбос", 4),
+                new Dog("Полкан", 5)
+        };
+        DogHelper.printAllNames(dogs);
+        double AverageAge = DogHelper.averageAge(dogs);
+        System.out.println(AverageAge);
+    }
 
 }
